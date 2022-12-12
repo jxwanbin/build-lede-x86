0336193f0696d77aba34eb67e91075f551b3e070
jxwanbin <74758217+jxwanbin@users.noreply.github.com>
2022-12-12 12:09:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,5 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Modify default kernel edition
+sed -i 's/5.15/6.0/g' target/linux/x86/Makefile
